7129df718fe5ac166e2d73ad438584acb9c02df7
Jazmin M <jazminmerckel@gmail.com>
2019-10-01 23:16:59 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,7 @@
-
-
+#each.do iterates over each element in array, word names element
+#next, reverse over each element 
 def return_each_word(sentence)
-  sentence.split(" ").each do |word| 
-
+  sentence.split(" ").each do |word|
+  end
+  
 end
